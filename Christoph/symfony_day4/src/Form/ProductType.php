17d4cf6922933ba821afa9d7cc260bb8dab20316
Christoph Murhammer <c.murhammer@hotmail.com>
2022-08-03 13:45:10 +0200

    added fileUploader and create page

---
+++ Christoph/symfony_day4/src/Form/ProductType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\FormBuilderInterface;
+use App\Entity\Product;
+use App\Service\FileUploader;
+
+class ProductType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+      $builder
+          ->add('name', TextType::class, [
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('price', IntegerType::class, [
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('picture', FileType::class, [
+              'label' => 'Upload picture',
+              'mapped' => 'false',
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px'],
+              'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'image/png',
+                        'image/jpeg',
+                        'image/jpg'
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid image file',
+                ])
+                ],
+          ])
+          ->add('fkSupplierID', ChoiceType::class, [
+              'choices' => ['Logitech' => '1', 'Razer' => '2'],
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('save', SubmitType::class, [
+              'label' => 'Submit',
+              'attr' => ['class' => 'btn-primary', 'style' => 'margin-bottom:15px']
+          ]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+      $resolver->setDefaults([
+          'data_class' => Product::class,
+      ]);
+  }
+}
